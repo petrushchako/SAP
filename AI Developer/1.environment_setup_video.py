f17a9374c89ef2300d40929c0837541875a7896a
Oleksandr Petrushchak <60898768+petrushchako@users.noreply.github.com>
2026-03-03 20:45:11 +0000

    Add video embedding functionality in environment setup

---
+++ AI Developer/1.environment_setup_video.py
@@ -0,0 +1,7 @@
+from IPython.display import IFrame
+
+# URL of the video you want to embed
+video_url = "https://video.sap.com/embed/secure/iframe/entryId/1_rrp2ogxr/uiConfId/54310412/st/0"
+
+# Embed the video using IFrame
+IFrame(src=video_url, width=768, height=432)
